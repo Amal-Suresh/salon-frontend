4075b9fbfa09e7bae96ffaaddd05966b6ce9fa8a
Amal-Suresh <amalsureshms2001@gmail.com>
2024-08-17 11:37:18 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="text-center bg-blue-500 text-white py-10">
+      <h1 className="text-3xl font-bold ">BookMySalon</h1>
+     
+    </div>
+  );
+}
+
+export default App;
